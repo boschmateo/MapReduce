bae652bfed4aee5a86afb7ef9f6c6c08b402122b
Roger Bosch <roger.bosch.mateo@gmail.com>
2018-03-21 18:33:13 +0100

    Scripts parametizados para

--- client.py
+++ client.py
@@ -2,6 +2,7 @@
 @author: Jeroni Molina Mellado
 @author: Roger Bosch Mateo
 '''
+import sys
 from pyactor.context import set_context, create_host, Host, sleep, shutdown
 from pyactor.exceptions import TimeoutError
 
@@ -10,10 +11,16 @@ class Server(object):
     #Synchronous (return method)
     _ask = ['reduce']
     #Asynchronous
-    _tell = ['map']
+    _tell = ['map', 'test']
 
-    def map(self, funtion_to_apply, input_item):
-        print "Map"
+    def map(self, function_to_apply, input_item):
+        if (function_to_apply == 'CW'):
+            countingWords(input_item)
+        elif (function_to_apply == 'WC'):
+            wordCounting(input_item)
+
+    def test(self, value):
+        print "I am "+str(value)
 
     def reduce(self, value):
         return value
@@ -21,15 +28,24 @@ class Server(object):
 
 if __name__ == "__main__":
     set_context()
+
+    remoteHostList = []
+    nSpawns=int(sys.argv[1])
+
     host = create_host('http://127.0.0.1:1679')
 
-    remote_host = host.lookup_url('http://127.0.0.1:1277/', Host)
-    slave = remote_host.spawn('slave', 'client/Server')
-    slave2 = remote_host.spawn('slave2', 'client/Server')
-    
-    slave.map("function", "input")
-    print slave2.reduce(4)
-    print slave.reduce(3)
+    #Spawn all the
+    for x in range(1277, 1277+nSpawns):
 
+        print "On port "+str(x)
+        #Get the host reference
+        remoteHost = host.lookup_url('http://127.0.0.1:'+str(x)+'/', Host)
+        #Add the slaves into the list
+        remoteHostList.append(remote_host.spawn(x, 'client/Server'))
+
+    #Testing the values
+    for x in range(nSpawns):
+        remoteHostList[x].test(x)
+    
     sleep(3)
     shutdown()
--- host.py
+++ host.py
@@ -2,13 +2,20 @@
 @author: Jeroni Molina Mellado
 @author: Roger Bosch Mateo
 '''
+import sys
 from pyactor.context import set_context, create_host, serve_forever
 
 
 if __name__ == "__main__":
+
     set_context()
-    host = create_host('http://127.0.0.1:1277/')
 
-    print 'host listening at port 1277'
-
+    if (len(sys.argv) > 1):
+        host = create_host('http://127.0.0.1:'+sys.argv[1]+'/')
+        print 'host listening at port '+sys.argv[1]
+    else:
+        print "****You are currently only running one host****"
+        host = create_host('http://127.0.0.1:1277/')
+        print 'host listening at port 1277'
+    
     serve_forever()
